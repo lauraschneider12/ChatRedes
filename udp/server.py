3655fd6c07ab8a1bc4c41e32e56fca5f26894b57
lauraschneider12 <lauraschneider1712s@gmail.com>
2026-05-01 20:53:19 -0300

    a

--- udp/server.py
+++ udp/server.py
@@ -1,34 +1,37 @@
 import socket
 
-def  start_server(host: str, port: int):
+def start_server(host: str, port: int):
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.bind((host,port))
+    server_socket.bind((host, port))
 
-    print(f'Server Started at {host}:{port} ')
+    print(f'Server Started at {host}:{port}')
+    
     usuarios = {}
 
     while True:
         data, addr = server_socket.recvfrom(1024)
+        decoded = data.decode('utf-8')
 
-        name = data.decode('utf-8').split('!#')[0]
-     
+        name, message = decoded.split('!#')
 
-        print('Usuários conectados:')
-        for user in usuarios.values():
-            print('-', user)
-        message = data.decode('utf-8').split('!#')[1]
+        
         if addr not in usuarios:
             usuarios[addr] = name
             print(f'{name} entrou no chat')
 
-        print(f'[{name}] diz: {message}')
+       
+        print('\nUsuários conectados:')
+        for user in usuarios.values():
+            print('-', user)
 
-        print(f'[{ name }] say: { message }')
+        print(f'Total: {len(usuarios)} usuário(s)')
 
-if __name__=='__main__':
-    HOST = '192.168.18.17'
-    # HOST = 'localhost'
+      
+        print(f'[{name}] diz: {message}\n')
+
+if _name_ == '_main_':
+    HOST = 'localhost'
     PORT = 8000
 
     start_server(HOST, PORT)
